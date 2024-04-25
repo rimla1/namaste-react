6da918564a3cde769fad77e9a2e1c12de75d2b88
Almir Muminovic <projectalmir@gmail.com>
2024-04-25 15:54:18 +0200

    Using react and react-dom directly from node_modules

--- Inception/app.js
+++ Inception/app.js
@@ -1,8 +1,11 @@
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const parent = React.createElement('div', { id: 'parent' }, [
   React.createElement('div', { id: 'child1' }, [
-    React.createElement('h1', {}, 'Im h1 tag'),
+    React.createElement('h1', {}, 'LMAO!'),
     React.createElement('h2', {}, 'Im h2 tag'),
   ]),
   React.createElement('div', { id: 'child2' }, [
