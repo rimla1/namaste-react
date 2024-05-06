3997a1aeae70ce63b780a9b2cf127f4be257139f
Almir Muminovic <projectalmir@gmail.com>
2024-05-06 14:12:04 +0200

    Filtering restaurants and searching restaurants

--- Inception/src/components/Body.js
+++ Inception/src/components/Body.js
@@ -4,37 +4,59 @@ import Shimmer from './Shimmer';
 
 export const Body = () => {
   const [restaurants, setRestaurants] = useState([]);
-
-
+  const [filteredRestaurants, setFilteredRestaurants] = useState([])
+  const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   const fetchData = async () => {
-    const data = await fetch("http://localhost:3000/restaurants")
-    const jsonData = await data.json()
-    console.log(jsonData)
-    setRestaurants(jsonData)
-  }
+    const data = await fetch('http://localhost:3000/restaurants');
+    const jsonData = await data.json();
+    setRestaurants(jsonData);
+    setFilteredRestaurants(jsonData)
+  };
 
-  return restaurants.length === 0 ? <Shimmer /> : (
+  return restaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className='body'>
       <div className='filter'>
+        <div className='search'>
+          <input
+            type='text'
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value)
+            }}
+          />
+          <button
+            className='search-btn'
+            onClick={() => {
+              const filteredRestaurants = restaurants.filter((restaurant) => {
+                return restaurant.data.name.toLowerCase().includes(searchText.toLocaleLowerCase())
+              });
+              setFilteredRestaurants(filteredRestaurants)
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className='filter-btn'
           onClick={() => {
             const filteredRestaurants = restaurants.filter(
               (restaurant) => restaurant.data.review > 4
             );
-            setRestaurants(filteredRestaurants);
+            setFilteredRestaurants(filteredRestaurants);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className='restaurantsContainer'>
-        {restaurants.map((restaurant) => (
+        {filteredRestaurants.map((restaurant) => (
           <Card key={restaurant.data.id} restaurantData={restaurant} />
         ))}
       </div>
