db1e5136a43e2f7d3d97bd05bc19d763c6db5688
Almir Muminovic <projectalmir@gmail.com>
2024-05-06 10:44:15 +0200

    Creating login/logout button functionality using useState hook

--- Inception/src/components/Header.js
+++ Inception/src/components/Header.js
@@ -1,21 +1,32 @@
 import { LOGO_URL } from '../utils/constants';
+import { useState } from 'react';
 
 export const Header = () => {
-    return (
-      <div className='header'>
-        <div className='logo-container'>
-          <img
-            className='logo'
-            src={LOGO_URL}
-          />
-        </div>
-        <div className='nav-items'>
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  let [buttonValue, setButtonValue] = useState('Login');
+
+  return (
+    <div className='header'>
+      <div className='logo-container'>
+        <img className='logo' src={LOGO_URL} />
       </div>
-    );
-  };
+      <div className='nav-items'>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className='login-btn'
+            onClick={() => {
+              buttonValue === 'Login'
+                ? setButtonValue('Logout')
+                : setButtonValue('Login');
+            }}
+          >
+            {buttonValue}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
